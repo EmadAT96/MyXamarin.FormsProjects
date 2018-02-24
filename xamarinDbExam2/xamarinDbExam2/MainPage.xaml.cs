215f1897a44210e586fc00615ef30f5a1f304d10
Emad_AT <emadataolahi@yahoo.com>
2018-02-24 04:23:26 -0800

    loading the projrct

---
+++ xamarinDbExam2/xamarinDbExam2/MainPage.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Microsoft.EntityFrameworkCore;
+
+namespace xamarinDbExam2
+{
+	public partial class MainPage : ContentPage
+	{
+		public MainPage()
+		{
+			InitializeComponent();
+            MyDB mdb = new MyDB();
+            mdb.Database.EnsureCreated();
+            mdb.MyPerson.Add( new Person {Id=1,Name="Emad",Age=22 });
+            mdb.SaveChanges();
+            var a = mdb.MyPerson.ToList();
+		}
+	}
+}
---
+++ xamarinDbExam2/xamarinDbExam2/MyDB.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace xamarinDbExam2
+{
+    public class MyDB : DbContext
+    {
+        public DbSet<Person> MyPerson { get; set; }
+       
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            
+            string dbFileName = "persons.db";
+            string fullDbPath = dbFileName;
+            fullDbPath = System.IO.Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Personal), dbFileName);
+            optionsBuilder.UseSqlite($"Filename={fullDbPath}", x => x.SuppressForeignKeyEnforcement());
+            base.OnConfiguring(optionsBuilder);
+        }
+
+        
+    }
+}
